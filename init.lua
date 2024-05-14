89805d94d712914f341b65469fcdae6cade7d661
isai1-on-a-pod-racer <isai10sanchez@gmail.com>
2024-05-13 21:23:02 -0600

    cleaned up init.lua

--- init.lua
+++ init.lua
@@ -1,34 +1,13 @@
---[[
-
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-If you don't know anything about Lua, I recommend taking some time to read through
-a guide. One possible example which will only take 10-15 minutes:
+--[[ 
+-- Welcome esteemed traveler to my rasp pi 2 neovim config
+10-15 min Lua guide:
   - https://learnxinyminutes.com/docs/lua/
 
-After understanding a bit more about Lua, you can use `:help lua-guide` as a
-reference for how Neovim integrates Lua.
+Use `:help lua-guide` as a reference for how Neovim integrates Lua.
 - :help lua-guide
 - (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
 Kickstart Guide:
-
   TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
 
   Next, run AND READ `:help`.
@@ -41,16 +20,6 @@ Kickstart Guide:
     MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
     which is very useful when you're not exactly sure of what you're looking for.
 
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
 If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
 --]]
 
@@ -61,7 +30,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true 
 
 -- NOTE: whenever we write ' require 'module' ' Neovim will look in the ~/.config/nvim/lua folder for the specific module/folder with submodules
 
